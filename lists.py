5857cec3cc2714f8bbb65a162e1508b2f749ddaf
Ryan <1018718155@qq.com>
2018-10-07 00:41:37 +0800

    scope_name_list

--- lists.py
+++ lists.py
@@ -6,3 +6,5 @@ gun_name_list = ['98k', 'akm', 'aug', 'awm', 'dp28', 'groza', 'm16', 'm24', 'm24
                  'mk47', 'qbu', 'qbz', 's12k', 's1987', 's686', 'scar', 'sks', 'slr', 'tommy', 'ump9', 'uzi', 'vector',
                  'vss', 'win94']
 
+
+scope_name_list = ['s1r', 's1h', 's2', 's3', 's4', 's6', 's8', 's15']
