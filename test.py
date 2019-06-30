d2ebb625341b5e6acce657678452500d204d087e
Ryan <1018718155@qq.com>
2019-06-30 16:22:27 +0800

    works well on test image

--- test.py
+++ test.py
@@ -3,19 +3,7 @@ import cv2
 from matplotlib import pyplot as plt
 
 
-def get_cross_kernel(corner_len=15, thick=1):
-    c_len = corner_len
-
-    cross_kernel = np.zeros((2*c_len+thick, 2*c_len+thick))
-    for i in range(c_len+thick):
-        div = thick*(2*c_len+thick)
-        for t in range(-thick//2, thick//2):
-            cross_kernel[t][t] = 1/div
-
-    return cross_kernel
-
-
-def get_rect_kernel(corner_len=30, thick=1):
+def get_rect_kernel(corner_len=30, thick=2):
     c_len = corner_len
 
     left_up_kernel = np.zeros((2*c_len+thick, 2*c_len+thick))
@@ -43,18 +31,14 @@ def get_rect_kernel(corner_len=30, thick=1):
 
 def find_corner_by_corner_canny(corner, canny):
     corner = cv2.filter2D(canny, -1, corner)
-    # corner = cv2.filter2D(corner, -1, get_cross_kernel())
-    # corner = cv2.filter2D(corner, -1, np.array([[1/4, 1/4], [1/4, 1/4]]))
-    # cv2.imshow("corner", cv2.equalizeHist((corner*255).astype(np.uint8)))
-    # cv2.waitKey()
     gray_sort = np.sort(corner.flatten())[::-1]
-    coordinate = np.where(corner > gray_sort[100])
+    coordinate = np.where(corner > gray_sort[300])
     coordinate = coordinate[::-1]
     coordinate = list(map(list, zip(*coordinate)))
     return coordinate  # (x, y)
 
 
-def get_possible_rectangle(lu_coord, ru_coord, rd_coord, ld_coord, img):
+def get_possible_rectangle(lu_coord, ru_coord, rd_coord, ld_coord):
     blur_thr = 2
     possible_rectangle = list()
     for a, b in lu_coord:
@@ -69,18 +53,17 @@ def get_possible_rectangle(lu_coord, ru_coord, rd_coord, ld_coord, img):
 
 
 if __name__ == '__main__':
-    # img = cv2.imread('D:/github_project/auto_press_down_gun/image_detect/screen_captures/weapon/akm/akm.png')
-    # img = cv2.imread('D:/github_project/auto_press_down_gun/image_detect/screen_captures/weapon/98k/98k.png')2188
-    img = cv2.imread('D:/github_project/auto_press_down_gun/image_detect/screen_captures/weapon/98k/98k.png')
-    img = img[120:170, 0:2238]
+    img = cv2.imread('D:/github_project/auto_press_down_gun/image_detect/screen_captures/weapon/akm/akm.png')
+    # img = cv2.imread('D:/github_project/auto_press_down_gun/image_detect/screen_captures/weapon/98k/98k.png')
+    # img = img[120:170, 0:2238]
     # cv2.imshow('img', img)
     # cv2.waitKey()
 
 
 
     canny = cv2.Canny(img, 30, 100)/255
-    # cv2.imshow("canny", canny)
-    # cv2.waitKey()
+    cv2.imshow("canny", canny)
+    cv2.waitKey()
 
     lu_c, ru_c, rd_c, ld_c = get_rect_kernel()
     lu_coor = find_corner_by_corner_canny(lu_c, canny)
@@ -88,29 +71,29 @@ if __name__ == '__main__':
     rd_coor = find_corner_by_corner_canny(rd_c, canny)
     ld_coor = find_corner_by_corner_canny(ld_c, canny)
 
-    # cp_img = img.copy()
-    # for x, y in lu_coor:
-        # cv2.circle(cp_img, (x,y), 1, (255, 0, 0), -1)
-    # for x, y in ru_coor:
-    #     cv2.circle(cp_img, (x,y), 1, (0, 255, 0), -1)
-    # for x, y in rd_coor:
-    #     cv2.circle(cp_img, (x, y), 1, (0, 0, 255), -1)
-    # for x, y in ld_coor:
-    #     cv2.circle(cp_img, (x,y), 1, (0, 255, 255), -1)
-    # cv2.imshow('img', cp_img)
-    # cv2.waitKey()
+    cp_img = img.copy()
+    for x, y in lu_coor:
+        cv2.circle(cp_img, (x,y), 1, (255, 0, 0), -1)
+    for x, y in ru_coor:
+        cv2.circle(cp_img, (x,y), 1, (0, 255, 0), -1)
+    for x, y in rd_coor:
+        cv2.circle(cp_img, (x, y), 1, (0, 0, 255), -1)
+    for x, y in ld_coor:
+        cv2.circle(cp_img, (x,y), 1, (0, 255, 255), -1)
+    cv2.imshow('img', cp_img)
+    cv2.waitKey()
 
-    possible_rect = get_possible_rectangle(lu_coor, ru_coor, rd_coor, ld_coor, img)
+    possible_rect = get_possible_rectangle(lu_coor, ru_coor, rd_coor, ld_coor)
 
     cp2_img = img.copy()
     for x, y, x1, y1 in possible_rect:
-        cv2.circle(cp2_img, (x, y), 5, (255, 0, 0), -1)
+        cv2.circle(cp2_img, (x, y), 1, (255, 0, 0), -1)
     for x, y, x1, y1 in possible_rect:
-        cv2.circle(cp2_img, (x1, y), 2, (0, 255, 0), -1)
+        cv2.circle(cp2_img, (x1, y), 1, (0, 255, 0), -1)
     for x, y, x1, y1 in possible_rect:
-        cv2.circle(cp2_img, (x1, y1), 2, (0, 0, 255), -1)
+        cv2.circle(cp2_img, (x1, y1), 1, (0, 0, 255), -1)
     for x, y, x1, y1 in possible_rect:
-        cv2.circle(cp2_img, (x, y1), 2, (0, 255, 255), -1)
+        cv2.circle(cp2_img, (x, y1), 1, (0, 255, 255), -1)
     cv2.imshow('cp2_img', cp2_img)
     cv2.waitKey()
 
