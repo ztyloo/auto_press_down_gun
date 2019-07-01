5b448c2a8a4b549b453f9a20cd61c223097e9fc6
Ryan <1018718155@qq.com>
2019-07-01 19:45:22 +0800

    label draw rearrange

--- auto_position_label/image_qlabel.py
+++ auto_position_label/image_qlabel.py
@@ -1,7 +1,7 @@
 from PyQt5 import QtWidgets
 from PyQt5 import QtCore
-from PyQt5 import QtGui
-from PyQt5.QtGui import QPainter, QColor, QBrush, QPen
+from PyQt5.QtCore import Qt, QRect, QPoint
+from PyQt5.QtGui import QPainter, QColor, QBrush, QPen, QFont, QPixmap
 import sys
 import numpy as np
 import cv2
@@ -13,28 +13,32 @@ class Image_QLabel(QtWidgets.QLabel):
     y, c_y, c_y0, c_y1 = 0, 0, 0, 0
 
     rect_n = 0
-    left_up = True
-
-    thr0, thr1 = 30, 300
-    c_dist = 1000000000
-    corner_br = 3
+    before_choose_one = True
+    shift_mode = False
 
     corner_rects = [(0, 0, 0, 0)] * 3
     res_rects = [(0, 0, 0, 0)] * 3
     len = 0
+    c_dist = 1000000000
+    corner_br = 3
+    circle_thr0, circle_thr1 = 17, 100
+
 
     def mousePressEvent(self, event):
-        if self.c_dist < self.thr0:
-            self.rect_n += 1
-            if self.rect_n == self.len:
-                self.rect_n = 0
-            if self.left_up:
-                self.c_x0, self.c_y0 = self.c_x, self.c_y
-                self.left_up = False
-            else:
-                self.c_x1, self.c_y1 = self.c_x, self.c_y
-                self.left_up = True
-                self.res_rects[self.rect_n] = (self.c_x0, self.c_y0, self.c_x1, self.c_y1)
+        if event.buttons() == Qt.LeftButton:
+            if self.c_dist < self.circle_thr0:
+                if self.before_choose_one:
+                    self.rect_n += 1
+                    if self.rect_n == self.len:
+                        self.rect_n = 0
+                    self.c_x0, self.c_y0 = self.c_x, self.c_y
+                    self.before_choose_one = False
+                else:
+                    self.c_x1, self.c_y1 = self.c_x, self.c_y
+                    self.before_choose_one = True
+                    self.res_rects[self.rect_n] = (self.c_x0, self.c_y0, self.c_x1, self.c_y1)
+        if event.buttons() == Qt.RightButton:
+            self.before_choose_one = True
 
     def mouseMoveEvent(self, event):
         self.x = event.x()
@@ -42,43 +46,13 @@ class Image_QLabel(QtWidgets.QLabel):
 
         self.c_dist = 1000000000
         for a, b, c, d in self.corner_rects:
-            temp_x, temp_y = (a, b) if self.left_up else (c, d)
+            temp_x, temp_y = (a, b) if self.before_choose_one else (c, d)
             temp_dist = np.linalg.norm(np.array([self.x, self.y]) - np.array([temp_x, temp_y])) // 1*2
             if temp_dist < self.c_dist:
                 self.c_dist = temp_dist
                 self.c_x, self.c_y = temp_x, temp_y
 
         self.update()
-
-    def paintEvent(self, event):
-        super().paintEvent(event)
-
-        abcd_painter = QPainter(self)
-        for a, b, c, d in self.corner_rects:
-            abcd_painter.setPen(QPen(QtCore.Qt.red,self.corner_br,QtCore.Qt.SolidLine))
-            abcd_painter.drawPoint(a, b)
-            abcd_painter.setPen(QPen(QtCore.Qt.blue,self.corner_br,QtCore.Qt.SolidLine))
-            abcd_painter.drawPoint(c, b)
-            abcd_painter.setPen(QPen(QtCore.Qt.green,self.corner_br,QtCore.Qt.SolidLine))
-            abcd_painter.drawPoint(c, d)
-            abcd_painter.setPen(QPen(QtCore.Qt.yellow,self.corner_br,QtCore.Qt.SolidLine))
-            abcd_painter.drawPoint(a, d)
-
-        rect_painter = QPainter(self)
-        for a, b, c, d in self.res_rects:
-            rect =QtCore.QRect(a, b, c-a, d-b)
-            rect_painter.setPen(QPen(QtCore.Qt.cyan,1,QtCore.Qt.SolidLine))
-            rect_painter.drawRect(rect)
-
-        circle_painter = QPainter(self)
-        circle_painter.setPen(QPen(QtCore.Qt.transparent, 0))
-        if self.thr0 < self.c_dist < self.thr1:
-            circle_painter.setBrush(QColor(255, 255, 0, 50))
-            circle_painter.drawEllipse(self.c_x-self.c_dist//2, self.c_y-self.c_dist//2, self.c_dist, self.c_dist)
-
-        if self.c_dist < self.thr0:
-            circle_painter.setBrush(QColor(0, 255, 0, 100))
-            circle_painter.drawEllipse(self.c_x-self.thr0//2, self.c_y-self.thr0//2, self.thr0, self.thr0)
 
     def setImage(self):
         fileName, _ = QtWidgets.QFileDialog.getOpenFileName(None, "Select Image", "", "Image Files (*.png)") # Ask for file
@@ -89,29 +63,70 @@ class Image_QLabel(QtWidgets.QLabel):
             self.len = len(corner_rects)
             self.res_rects = [(0, 0, 0, 0)] * self.len
 
-            pixmap = QtGui.QPixmap(fileName)  # Setup pixmap with the provided image
-            # pixmap = pixmap.scaled(self.width(), self.height(), QtCore.Qt.KeepAspectRatio) # Scale pixmap
+            pixmap = QPixmap(fileName)  # Setup pixmap with the provided image
+            # pixmap = pixmap.scaled(self.width(), self.height(), Qt.KeepAspectRatio) # Scale pixmap
             self.setPixmap(pixmap) # Set the pixmap onto the label
-            self.setAlignment(QtCore.Qt.AlignLeading) # Align the label to center
+            self.setAlignment(Qt.AlignLeading) # Align the label to center
 
+    def paintEvent(self, event):
+        super().paintEvent(event)
+        qp = QPainter(self)
 
-class Example(QtWidgets.QWidget):
-    def __init__(self):
-        super().__init__()
-        self.initUI()
+        if not self.before_choose_one:
+            self.draw_rect(qp, (self.c_x0, self.c_y0, self.c_x, self.c_y), (0,255,255))
+        for rect in self.corner_rects:
+            self.draw_rect_corner(qp, rect)
+        for rect in self.res_rects:
+            self.draw_rect(qp, rect, (0,255,0))
+            self.draw_coordinate(qp, (rect[0], rect[1]), (rect[0], rect[1]), (255, 0, 255))
+            self.draw_coordinate(qp, (rect[2], rect[3]), (rect[2], rect[3]), (255, 0, 255))
 
-    def initUI(self):
-        self.resize(3000, 1300)
-        self.setWindowTitle('在label中绘制矩形')
-        self.label = Image_QLabel(self)  #重定义的label
-        self.label.setGeometry(QtCore.QRect(30, 30, 2000, 1000))
+        if self.circle_thr1 < self.c_dist:
+            self.draw_cross(qp, (self.x, self.y))
+            self.draw_coordinate(qp, (self.x, self.y), (self.x, self.y), (255, 0, 255))
+        if self.circle_thr0 < self.c_dist < self.circle_thr1:
+            self.draw_circle(qp, (self.c_x, self.c_y), self.c_dist, (255, 255, 0, 100))
+            self.draw_cross(qp, (self.x, self.y))
+            self.draw_coordinate(qp, (self.x, self.y), (self.x, self.y), (255, 0, 255))
+        if self.c_dist < self.circle_thr0:
+            self.draw_circle(qp, (self.c_x, self.c_y), self.circle_thr0, (0, 255, 0, 100))
+            self.draw_cross(qp, (self.c_x, self.c_y))
+            self.draw_coordinate(qp, (self.c_x, self.c_y), (self.c_x, self.c_y), (255, 0, 255))
 
-        self.label.setImage()
+    def draw_circle(self, qp, xy, r, color=(255, 0, 255, 100)):
+        qp.setPen(QPen(Qt.transparent, 0))
+        qp.setBrush(QColor(*color))
+        qp.drawEllipse(QPoint(*xy), r, r)
 
-        self.show()
+    def draw_cross(self, qp, xy, color=(255, 0, 255, 100)):
+        x, y = xy
+        lenth = 1000
+        pen = QPen(QColor(*color), 1, Qt.SolidLine)
+        qp.setPen(pen)
 
+        qp.drawLine(x-lenth, y, x+lenth, y)
+        qp.drawLine(x, y-lenth, x, y+lenth)
 
-if __name__ == '__main__':
-    app = QtWidgets.QApplication(sys.argv)
-    x = Example()
-    sys.exit(app.exec_())
+    def draw_coordinate(self, qp, xy, xy_num, color=(255, 0, 255)):
+        x, y = xy
+        x_num, y_num = xy_num
+        qp.setPen(QColor(*color))
+        qp.setFont(QFont('Decorative', 10))
+        qp.drawText(QRect(x-50, y-20, 100, 15), 10, '('+str(x_num)+','+str(y_num)+')')
+
+    def draw_rect(self, qp, rect, color=(255, 0, 255)):
+        x0, y0, x1, y1 = rect
+        rect = QtCore.QRect(x0, y0, x1 - x0, y1 - y0)
+        qp.setPen(QPen(QColor(*color), 1, Qt.SolidLine))
+        qp.drawRect(rect)
+
+    def draw_rect_corner(self, qp, rect):
+        x0, y0, x1, y1 = rect
+        qp.setPen(QPen(Qt.red, self.corner_br, Qt.SolidLine))
+        qp.drawPoint(x0, y0)
+        qp.setPen(QPen(Qt.blue, self.corner_br, Qt.SolidLine))
+        qp.drawPoint(x1, y0)
+        qp.setPen(QPen(Qt.green, self.corner_br, Qt.SolidLine))
+        qp.drawPoint(x1, y1)
+        qp.setPen(QPen(Qt.yellow, self.corner_br, Qt.SolidLine))
+        qp.drawPoint(x0, y1)
--- auto_position_label/start_label.py
+++ auto_position_label/start_label.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'D:\github_project\auto_press_down_gun\auto_position_label\start_label.ui'
+# Form implementation generated from reading ui file 'D:\github_project\auto_press_down_gun\auto_position_label\start_label__.ui'
 #
 # Created by: PyQt5 UI code generator 5.11.3
 #
@@ -50,33 +50,31 @@ class Deep_vs_Wide_Dict:
 class Ui_Dialog(object):
     def setupUi(self, Dialog):
         Dialog.setObjectName("Dialog")
-        Dialog.resize(3440, 1370)
+        Dialog.resize(3440, 1350)
         Dialog.setMouseTracking(True)
-
-        self.radioButton = QtWidgets.QRadioButton(Dialog)
-        self.radioButton.setGeometry(QtCore.QRect(20, 80, 89, 16))
-        self.radioButton.setObjectName("radioButton")
-        self.radioButton_2 = QtWidgets.QRadioButton(Dialog)
-        self.radioButton_2.setGeometry(QtCore.QRect(20, 120, 89, 16))
-        self.radioButton_2.setObjectName("radioButton_2")
-        self.radioButton_3 = QtWidgets.QRadioButton(Dialog)
-        self.radioButton_3.setGeometry(QtCore.QRect(20, 160, 89, 16))
-        self.radioButton_3.setObjectName("radioButton_3")
+        self.startButton = QtWidgets.QPushButton(Dialog)
+        self.startButton.setGeometry(QtCore.QRect(10, 0, 111, 31))
+        self.startButton.setObjectName("startButton")
+        self.stopButton = QtWidgets.QPushButton(Dialog)
+        self.stopButton.setGeometry(QtCore.QRect(10, 1320, 111, 31))
+        self.stopButton.setObjectName("stopButton")
 
         self.label = Image_QLabel(Dialog)
-        self.label.setGeometry(QtCore.QRect(120, 10, 3311, 1421))
+        self.label.setGeometry(QtCore.QRect(120, 0, 3311, 1431))
         self.label.setFrameShape(QtWidgets.QFrame.Box)
         self.label.setText("")
         self.label.setObjectName("label")
         self.label.setMouseTracking(True)
 
-        self.startButton = QtWidgets.QPushButton(Dialog)
-        self.startButton.setGeometry(QtCore.QRect(10, 10, 111, 51))
-        self.startButton.setObjectName("pushButton")
-        self.endButten = QtWidgets.QPushButton(Dialog)
-        self.endButten.setEnabled(True)
-        self.endButten.setGeometry(QtCore.QRect(10, 1380, 111, 51))
-        self.endButten.setObjectName("pushButton_2")
+        dvw_dict = Deep_vs_Wide_Dict('x0')
+        dvw_dict.d_dict = screen_position
+        dvw_dict.d_to_w()
+        self.w_dict = dvw_dict.w_dict
+
+        self.radioButtons = [QtWidgets.QRadioButton(Dialog)]*len(self.w_dict)
+        for i, k in enumerate(self.w_dict):
+            self.radioButtons[i].setGeometry(QtCore.QRect(20, 80+20*i, 89, 16))
+            self.radioButtons[i].setObjectName(k)
 
         self.retranslateUi(Dialog)
         QtCore.QMetaObject.connectSlotsByName(Dialog)
@@ -86,22 +84,23 @@ class Ui_Dialog(object):
     def retranslateUi(self, Dialog):
         _translate = QtCore.QCoreApplication.translate
         Dialog.setWindowTitle(_translate("Dialog", "Locate_rectangle"))
-        self.radioButton.setText(_translate("Dialog", "RadioButton"))
         self.startButton.setText(_translate("Dialog", "Start"))
-        self.endButten.setText(_translate("Dialog", "finish"))
+        self.stopButton.setText(_translate("Dialog", "Stop"))
+        for i, k in enumerate(self.w_dict):
+            self.radioButtons[i].setText(_translate("Dialog", k))
 
 
 if __name__ == "__main__":
-    d = Deep_vs_Wide_Dict('x0')
-    d.d_dict = screen_position
-    d.d_to_w()
-    d.w_to_d()
+    # d = Deep_vs_Wide_Dict('x0')
+    # d.d_dict = screen_position
+    # d.d_to_w()
+    # d.w_to_d()
 
-    # import sys
-    # app = QtWidgets.QApplication(sys.argv)
-    # Dialog = QtWidgets.QDialog()
-    # ui = Ui_Dialog()
-    # ui.setupUi(Dialog)
-    # Dialog.show()
-    # sys.exit(app.exec_())
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    Dialog = QtWidgets.QDialog()
+    ui = Ui_Dialog()
+    ui.setupUi(Dialog)
+    Dialog.show()
+    sys.exit(app.exec_())
 
