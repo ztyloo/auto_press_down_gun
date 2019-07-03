125ef14365913ad401fac34a2199ade56012d1e6
Ryan <1018718155@qq.com>
2019-07-03 15:40:34 +0800

    Key_Listener init done

--- auto_position_label/1_auto_screen_capture.py
+++ auto_position_label/1_auto_screen_capture.py
@@ -7,7 +7,6 @@ from PIL import ImageGrab
 
 from auto_position_label.crop_position import screen_position_states
 from show_watermark import Show_Watermark
-from utils.utils import Deep_vs_Wide_Dict
 
 
 def get_screen():
@@ -22,23 +21,31 @@ class Key_Listener(PyKeyboardEvent):
         PyKeyboardEvent.__init__(self)
         root_path = 'D:/github_project/auto_press_down_gun/auto_position_label/screen_captures'
         os.makedirs(root_path, exist_ok=True)
+
+        self.path_strs = list()
+        self.escape_c = '/'
         for k, v in screen_position_states.items():
             if len(v) > 0:
                 for state in v:
                     state_fold = os.path.join(root_path, state)
                     os.makedirs(state_fold, exist_ok=True)
 
+                    self.path_strs.append(k + self.escape_c + state)
+
+        self.state_num = len(self.path_strs)
         self.all_states = all_states
         self.show = Show_Watermark()
 
-        self.dvw_dict = Deep_vs_Wide_Dict()
-        self.dvw_dict.d_dict = screen_position_states
-        self.dvw_dict.d_to_w()
-        self.pos_states = self.dvw_dict.w_dict
 
     def tap(self, keycode, character, press):
         if keycode == 162 and press:  # ctrl
             self.alt_func()
+
+        if keycode == 37 and press:  # <-
+            self.left_func()
+
+        if keycode == 39 and press:  # ->
+            self.right_func()
 
         if keycode == 164 and press:  # alt
             self.alt_func()
@@ -48,6 +55,12 @@ class Key_Listener(PyKeyboardEvent):
         return False
 
     def ctrl_func(self):
+        pass
+
+    def left_func(self):
+        pass
+
+    def right_func(self):
         pass
 
     def alt_func(self):
