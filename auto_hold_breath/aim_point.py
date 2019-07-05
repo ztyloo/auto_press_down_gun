ae67a1a6806b5324e3419cdbf7c502912db2322f
Ryan <1018718155@qq.com>
2019-07-05 13:35:07 +0800

    tracking need design

--- auto_hold_breath/aim_point.py
+++ auto_hold_breath/aim_point.py
@@ -24,25 +24,12 @@ class Aim_Point:
         _, labels, stats, centroids = cv2.connectedComponentsWithStats(hole_position)
         return int(centroids[-1][0]), int(centroids[-1][1])
 
-    def get_6_aim_point_kernel(self, radius=300):
-        thickness = 4
-        aim_point_kernel = np.zeros((2 * radius + thickness, 2 * radius + thickness))
-
-        area = 0
-        for j in range(-2 * radius, 2 * radius + 1):
-            for i in range(-2 * radius, 2 * radius + 1):
-                if radius < i <= radius + thickness or radius < j <= radius + thickness:
-                    aim_point_kernel[i, j] = 1
-                    area += 1
-        aim_point_kernel /= area
-        return aim_point_kernel
-
 
 if __name__ == '__main__':
     i = 4
     radius = 100
     screen = cv2.imread('D:/github_project/auto_press_down_gun/auto_hold_breath/kernal_screen/'+str(i)+'.png')
-    screen = screen[720-radius:720+radius, 1720-radius:1720+radius]
+    screen = screen[719-radius:719+radius, 1719-radius:1719+radius]
 
     aim_point = Aim_Point()
     x0, y0 = aim_point.search_aim_point(screen, str(i))
---
+++ auto_hold_breath/tracking_aim_point.py
@@ -0,0 +1,47 @@
+import threading
+import win32api
+import win32con
+import time
+
+from screen_capture import win32_cap
+from auto_hold_breath.aim_point import Aim_Point
+
+
+def mouse_move(dx, dy):
+    win32api.mouse_event(win32con.MOUSEEVENTF_MOVE, int(dx), int(dy))
+
+
+class Tracking_Aim_Point(threading.Thread):
+    def __init__(self, scope):
+        threading.Thread.__init__(self)
+        self.scope = str(scope)
+        self._loop = True
+        self.aim_point = Aim_Point()
+
+    def run(self) -> None:
+        radius = 100
+        temp_path = 'D:/github_project/auto_press_down_gun/auto_hold_breath/temp_fold/temp.png'
+        im = win32_cap(temp_path, (1719 - radius, 719 - radius, 1719 + radius, 719 + radius))
+        x0, y0 = self.aim_point.search_aim_point(im, self.scope)
+        i = 0
+        while self._loop:
+            radius = 100
+            temp_path = 'D:/github_project/auto_press_down_gun/auto_hold_breath/temp_fold/'+str(i)+'.png'
+            im = win32_cap(temp_path, (1719-radius, 719-radius,1719+radius, 719+radius))
+            x1, y1 = self.aim_point.search_aim_point(im, self.scope)
+            dx, dy = x0 - x1, y0 - y1
+            print(dx, dy)
+            dx = dx if dx<10 else 0
+            dy = dy if dy<10 else 0
+            mouse_move(dx, dy)
+            x0, y0 = x1, y1
+
+            i = i+1 if i<20 else 0
+
+    def stop(self):
+        self._loop = False
+
+
+if __name__ == '__main__':
+    tracking_aim_point = Tracking_Aim_Point(6)
+    tracking_aim_point.run()
